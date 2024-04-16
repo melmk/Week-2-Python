d66ea7fa2920cb21b68384ae58ddefcf7249fe5c
MMK <mmkcodes@gmail.com>
2024-04-16 23:58:53 +1000

    added comments

--- flight.py
+++ flight.py
@@ -1,8 +1,10 @@
 class Flight():
+    # Creating object
     def __init__(self, capacity):
         self.capacity = capacity
         self.passengers = [] # Starts with an empty list of passengers
 
+    # Method that adds passengers to flight so long as there is room on the plane
     def addPassenger(self, name):
         if not self.openSeats():       # pythonic way of doing if self.openSeats == 0
             return False
@@ -10,16 +12,21 @@ class Flight():
             self.passengers.append(name)
             return True
 
+    # Returns number of open seats remaining
     def openSeats(self):
         return self.capacity - len(self.passengers)
 
+# Create the flight
 flight = Flight(3)
 
+# Create list of people
 people = ["Harry", "Ron", "Hermione", "Draco"]
 
+# Try to add each person to the flight
 for person in people:
     success = flight.addPassenger(person)
 
+    # Print messages depending on success of adding to flight
     if success:
         print(f"Added {person} to flight successfully")
     else:
