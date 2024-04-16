fcb8b1def572297d93cf75c2842b2cc4a432cac9
MMK <mmkcodes@gmail.com>
2024-04-16 23:09:13 +1000

    first comit

---
+++ lists.py
@@ -0,0 +1,10 @@
+# Creating a list of names
+names = ["harry", "ron", "hermione", "ginny"]
+
+print(names[0])
+
+# methods/functions of lists
+names.append("draco")
+names.sort(reverse=True)
+
+print(names)
