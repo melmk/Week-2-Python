b063adf9597de9f0abb6285df72bb83c01e4b02a
MMK <mmkcodes@gmail.com>
2024-04-16 23:20:22 +1000

    first commit

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+houses = {"harry":"gryffindor", 
+          "draco":"slytherin",
+          "cho":"ravenclaw",
+          "cedric":"hufflepuff"}
+
+print(f"Harry is in {houses["harry"]}")
+
+
+# Adds values in dictionary
+houses["hermione"] = "gryffindor"
+print(houses["hermione"])
