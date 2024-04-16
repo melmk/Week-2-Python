89db9e7c783b2422e6bc53b0f203711b53a00ee6
MMK <mmkcodes@gmail.com>
2024-04-16 23:04:52 +1000

    first commit

---
+++ sequences.py
@@ -0,0 +1,17 @@
+# Strings are arrays of chars
+name = "harry"
+print(name[0])
+
+for l in name:
+    print(l)
+
+# Lists - Mutable. Order Matters. Non-unique values 
+names = ["harry", "ron", "hermione"]
+print(names)
+
+# Tuple - Immutable. For a pair or so of values that are referenced as a unit. Non-unique values.
+coordinate = (10.0, 20.0) #parentheses for tuples instead of square brackets
+
+# Sets - Collection of unique values. Order doesn't matter. More efficient than lists.
+
+# Dictionaries (dict) - Key-Value pairs
