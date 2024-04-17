f5267a00d3057c6eb893945c36f606031697b6f7
MMK <mmkcodes@gmail.com>
2024-04-17 15:04:18 +1000

    first commit

---
+++ decorators.py
@@ -0,0 +1,21 @@
+# A decorator enables us to modify functions
+# It is a function that takes another function as an input and returns a modified version of the function as its output
+# This is a functional programming paradigm (functions are like all other things, just a value that can be parsed)
+
+# Takes some generic function, f, as input (decorator)
+def announce(f):
+    # Decorator defines a wrapper function which does the modifying
+    def wrapper():
+        print("About to run the function...")
+        f()
+        print("Done with the function.")
+    # returns the wrapper function (i.e calls)
+    return wrapper
+
+# Calls the decorator to apply to the function below
+@announce
+def hello():
+    print("Hello, world")
+
+# Run the function
+hello()
